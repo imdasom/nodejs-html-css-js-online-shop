1b50d8c84ec74328481dbe6fc293df1cd36f758f
imdasom <imdasom101@gmail.com>
2023-01-25 19:39:58 +0900

    대표이미지추가

--- client/layout/index.js
+++ client/layout/index.js
@@ -1,3 +1,13 @@
+function getParentElementByClass(element, className) {
+  if (element === null) {
+    return null;
+  }
+  if (element.classList.contains(className)) {
+    return element;
+  }
+  return getParentElementByClass(element.parentElement, className);
+}
+
 document.addEventListener('click', function onClickMenuItem(event) {
   if (event.target.classList.contains('menu-child')) return;
   const target = getParentElementByClass(event.target, 'menu-item');
--- server/controllers/ProductController.js
+++ server/controllers/ProductController.js
@@ -43,14 +43,13 @@ function ProductController() {
         }
 
         if (method === 'POST') {
-
           let body = '';
           req.on('data', function (data) {
             body += data;
             // Too much POST data, kill the connection!
             // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
             if (body.length > 1e6)
-              throw new Error(JSON.stringify({ statusCode: 500, message: 'EXCEED_BODY_CONTENET_LENGTH' }));
+              throw new Error(JSON.stringify({ statusCode: 500, message: 'EXCEED_BODY_CONTENT_LENGTH' }));
           });
           req.on('end', function () {
             try {
@@ -90,7 +89,7 @@ function ProductController() {
             // Too much POST data, kill the connection!
             // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
             if (body.length > 1e6)
-              throw new Error(JSON.stringify({ statusCode: 500, message: 'EXCEED_BODY_CONTENET_LENGTH' }));
+              throw new Error(JSON.stringify({ statusCode: 500, message: 'EXCEED_BODY_CONTENT_LENGTH' }));
           });
           req.on('end', function () {
             const params = JSON.parse(body);
